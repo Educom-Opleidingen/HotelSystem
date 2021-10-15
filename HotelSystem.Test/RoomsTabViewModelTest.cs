128b75f156c757e36f2dc42bc3c2f7fd41383a6d
Jeroen Heemskerk <j.w.heemskerk@gmail.com>
2021-10-15 12:52:42 +0200

    Introduced Setup & Teardown and added code to ease use of propertyChanges

--- HotelSystem.Test/RoomsTabViewModelTest.cs
+++ HotelSystem.Test/RoomsTabViewModelTest.cs
@@ -2,29 +2,67 @@
 using HotelSystem.Model;
 using HotelSystem.ViewModel;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 
 namespace HotelSystem.Test
 {
     public class RoomsTabViewModelTest
     {
-        readonly List<string> propertyChanges = new List<string>();
+        private TestRoomRepository repository;
+        private RoomsTabViewModel rtvm;
+
+        #region PropertyChanges
+        private readonly List<string> propertyChanges = new List<string>();
 
         private void PropertyChangedHandler(object sender, PropertyChangedEventArgs e)
         {
             propertyChanges.Add(e.PropertyName);
         }
 
+        private void AssertPropertyChanged(string expectedProperty)
+        {
+            Assert.IsNotEmpty(propertyChanges, 
+                              string.Format("Expected property change {0} not fired", expectedProperty));
+            int index = propertyChanges.IndexOf(expectedProperty);
+            propertyChanges.Remove(expectedProperty); // Remove it from the list as it was expected
+
+            if (index < 0) // Not in the list
+            {
+                Assert.Fail(string.Format("Expected property change {0} not fired", expectedProperty));
+            } else if (index > 0)  // Not the first in the list, report the once before it
+                Assert.IsEmpty(propertyChanges.Take(index), 
+                               string.Format("Found other properties changed before the property change of {0}", 
+                                             expectedProperty));
+        }
+        #endregion
+
+        #region Setup
+        [SetUp]
+        public void RunBeforeEachTest()
+        {
+            repository = new TestRoomRepository();
+            rtvm = new RoomsTabViewModel(repository);
+            rtvm.PropertyChanged += PropertyChangedHandler;
+        }
+
+        [TearDown]
+        public void RunAfterEachTest()
+        {
+            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Passed)
+            {
+                Assert.IsEmpty(propertyChanges, "Unexpected Property Changes to the folowing Properties:");
+            }
+        } 
+        #endregion
+
         [Test]
         public void TestSetSelectedRoom()
         {
             // prepare
-            IRoomRepository repository = new TestRoomRepository();
-            RoomsTabViewModel rtvm = new RoomsTabViewModel(repository);
-            rtvm.PropertyChanged += PropertyChangedHandler;
-
             Room testRoom = new Room() { Number = "123", Type = RoomTypes.StandardRoom };
 
             // run
@@ -35,7 +73,7 @@ namespace HotelSystem.Test
             Assert.NotNull(result);
             Assert.AreEqual(testRoom.Number, result.Number);
             Assert.AreEqual(testRoom.Type, result.Type);
-            Assert.Contains(nameof(rtvm.SelectedRoom), propertyChanges);
+            AssertPropertyChanged(nameof(rtvm.SelectedRoom));
         }
     }
 }
